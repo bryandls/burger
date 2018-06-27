d8d1874ff3d9589e75e0d52a73e2aa9fae3d2b73
bryandls <bryandls86@gmail.com>
2018-06-27 00:00:46 -0700

    push

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Hamburger");
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("VeggieBurger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("TruffleBurger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", true);
+INSERT INTO burgers (burger_name) VALUES ("Lamb Burger");
